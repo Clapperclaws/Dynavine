525ed22ffd619cff040f1c82a7f3e7da518a9889
saraa <saraa@DESKTOP-0NK9M8V>
2017-06-04 15:34:22 -0400

    Read IP Graph

--- src/Driver.java
+++ src/Driver.java
@@ -6,18 +6,16 @@ import java.util.ArrayList;
 
 public class Driver {
 
-	public static void main(String [] args){
+	public static void main(String [] args) throws IOException{
 		
 		//Initialize OTN Graph
-		Graph otn;
-		try {
-			otn = ReadFromFile("./src/Dataset/OTN", EndPoint.type.otn);
-			System.out.println("******* OTN Graph ******* \n"+otn); // Print graph for testing
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		Graph otn = ReadFromFile("./src/Dataset/OTN", EndPoint.type.otn);
+		System.out.println("******* OTN Graph ******* \n"+otn); // Print graph for testing
+	
+		//Initialize IP Graph
+		Graph ip  = ReadFromFile("./src/Dataset/IP", EndPoint.type.ip);
+		System.out.println("******* IP Graph ******* \n"+ip); // Print graph for testing
+	
 		
 	}
 	
@@ -28,12 +26,13 @@ public class Driver {
 	    try {
 	        StringBuilder sb = new StringBuilder();
 	        String line = br.readLine();
-	        g = new Graph(Integer.parseInt(line)); //Initialize Graph
+	        g = new Graph(Integer.parseInt(line)); //Initialize Graph with the number of Nodes
 	        int counter = 0;
 	        while (line != null) {
 	            line = br.readLine();
 	            if(line != null){
-	            	String[] splitLine = line.split(","); //Split to get NodeID and Bw.
+	            	//Split the line to get 0: index of the first node; 1: index of the second node; 2: bandwidth
+	            	String[] splitLine = line.split(",");
 	            	//Create two end points
 	            	EndPoint ep1 = new EndPoint(Integer.parseInt(splitLine[1]),Integer.parseInt(splitLine[2]),graphType,0);
 	            	g.addEndPoint(Integer.parseInt(splitLine[0]), ep1);
