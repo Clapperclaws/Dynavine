79293575429f95ea29b9d86929ab91d08e9dbc55
saraa <saraa@DESKTOP-0NK9M8V>
2017-06-04 17:06:23 -0400

    - Dijsktra Class
    - VN File

---
+++ src/Dijkstra.java
@@ -0,0 +1,113 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+
+
+/**
+ * 
+ * @author saraa
+ *  This class runs Dijkstra to find the shortest path between a pair of nodes
+ */
+public class Dijkstra {
+
+	private Graph g;
+    private Set<Integer> settledNodes;
+    private Set<Integer> unSettledNodes;
+    private Map<Integer, Integer> predecessors;
+    private Map<Integer, Integer> distance;
+
+    public Dijkstra(Graph graph) {
+       g = new Graph(graph); // Create a copy of the graph
+    }
+    
+    public void execute(int source) {
+        settledNodes = new HashSet<Integer>();
+        unSettledNodes = new HashSet<Integer>();
+        distance = new HashMap<Integer, Integer>();
+        predecessors = new HashMap<Integer, Integer>();
+        distance.put(source, 0);
+        unSettledNodes.add(source);
+        while (unSettledNodes.size() > 0) {
+            int node = getMinimum(unSettledNodes);
+            settledNodes.add(node);
+            unSettledNodes.remove(node);
+            findMinimalDistances(node);
+        }
+    }
+
+    
+    private void findMinimalDistances(int node) {
+        List<Integer> adjacentNodes = getNeighbors(node);
+        for (int target : adjacentNodes) {
+            if (getShortestDistance(target) > getShortestDistance(node) + g.getCost(node, target)) {
+                distance.put(target, getShortestDistance(node)+ g.getCost(node, target));
+                predecessors.put(target, node);
+                unSettledNodes.add(target);
+            }
+        }
+    }
+    
+    private List<Integer> getNeighbors(int node) {
+        List<Integer> neighbors = new ArrayList<Integer>();
+        for (int i=0;i<g.getAllEndPoints(node).size();i++) {
+            if (!isSettled(g.getAllEndPoints(node).get(i).getNodeId())) {
+                neighbors.add(g.getAllEndPoints(node).get(i).getNodeId());
+            }
+        }
+        return neighbors;
+    }
+
+    private int getMinimum(Set<Integer> vertexes) {
+        int minimum = Integer.MAX_VALUE;
+        for (int vertex : vertexes) {
+            if (minimum == Integer.MAX_VALUE) {
+                minimum = vertex;
+            } else {
+                if (getShortestDistance(vertex) < getShortestDistance(minimum)) {
+                    minimum = vertex;
+                }
+            }
+        }
+        return minimum;
+    }
+
+    private boolean isSettled(int vertex) {
+        return settledNodes.contains(vertex);
+    }
+
+    private int getShortestDistance(int destination) {
+        Integer d = distance.get(destination);
+        if (d == null) {
+            return Integer.MAX_VALUE;
+        } else {
+            return d;
+        }
+    }
+
+    /*
+     * This method returns the path from the source to the selected target and
+     * NULL if no path exists
+     */
+    public LinkedList<Integer> getPath(int target) {
+        LinkedList<Integer> path = new LinkedList<Integer>();
+        int step = target;
+        // check if a path exists
+        if (predecessors.get(step) == null) {
+            return null;
+        }
+        path.add(step);
+        while (predecessors.get(step) != null) {
+            step = predecessors.get(step);
+            path.add(step);
+        }
+        // Put it into the correct order
+        Collections.reverse(path);
+        return path;
+    }
+	
+}
