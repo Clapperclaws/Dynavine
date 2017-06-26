9a93baf03f8623a2985c26adcd1988b5c8b7a044
Shihabur Rahman Chowdhury <sr2chowdhury@uwaterloo.ca>
2017-06-25 22:58:06 -0400

    Fix shuffler.

--- src/shuffler.sh
+++ src/shuffler.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for testcase in (case.9.15.30 case.12.20.30 case.15.25.30 case.18.30.30 case.21.35.30)
+for testcase in 'case.9.15.30' 'case.12.20.30' 'case.15.25.30' 'case.18.30.30' 'case.21.35.30'
 do
-  for num_shuffles in (25 50 75 100)
+  for num_shuffles in 25 50 75 100
   do
     python run_experiment.py --executable ./ml_vne --testcase_root $testcase --num_shuffles $num_shuffles
     cp -r $testcase $testcase.$num_shuffles
